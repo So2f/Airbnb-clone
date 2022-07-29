fddcfd20351a9d65c7d55c3fdce1499173c16303
So2f <mastershouf91@gmail.com>
2022-07-29 02:32:32 +0200

    Airbnb clone

---
+++ components/Footer.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About</h5>
+        <p>How it works</p>
+        <p>News</p>
+        <p>Investor</p>
+        <p>Rbnb +</p>
+        <p>Rbnb luxe</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About</h5>
+        <p>How it works</p>
+        <p>News</p>
+        <p>Investor</p>
+        <p>Rbnb +</p>
+        <p>Rbnb luxe</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About</h5>
+        <p>How it works</p>
+        <p>News</p>
+        <p>Investor</p>
+        <p>Rbnb +</p>
+        <p>Rbnb luxe</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About</h5>
+        <p>How it works</p>
+        <p>News</p>
+        <p>Investor</p>
+        <p>Rbnb +</p>
+        <p>Rbnb luxe</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About</h5>
+        <p>How it works</p>
+        <p>News</p>
+        <p>Investor</p>
+        <p>Rbnb +</p>
+        <p>Rbnb luxe</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
